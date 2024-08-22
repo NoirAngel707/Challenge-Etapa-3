7fe143fed321b8fec147eeff2daabe6838c16beb
Jose  Lucas <97398370+Runado@users.noreply.github.com>
2024-08-22 20:59:37 -0300

    User interaction

--- AwsLexIntents.py
+++ AwsLexIntents.py
@@ -1,20 +1,20 @@
-#aws lambda code for 
-#How to Make a Chatbot Using Amazon Lex and AWS Lambda (Python) | Conversational AI Part 2
-# https://youtu.be/W6T-RFei6SY
 import json
 import datetime
 import time
-
+import whois
+from datetime import datetime
 
 def lambda_handler(event, context):
-#Função que recebe o nome da Intent e o tipo de evento
+    
     intent = event['sessionState']['intent']['name']
-#Linha que recebe o nome dos slots e os valores do mesmo.
     slots = event['sessionState']['intent']['slots']
-#Essa condição ela é atendida sempre que um evento de FulfillmentCodeHook for recebida, no Amazon Lex definimos que após todos os slots forem preenchidos um Evento de Fulfillment é enviado
+    
     if event['invocationSource'] == 'FulfillmentCodeHook':
-     
-   
+        celular = slots['celular']['value']['originalValue']
+        # Acessando o valor de 'originalValue' do campo 'URL'
+        URL = slots['URL']['value']['originalValue']
+        # Função para isolar os valores de 'originalValue'
+        resultado = whois.whois(URL)
         response = {
         "sessionState": {
             "dialogAction": {
@@ -26,14 +26,16 @@ def lambda_handler(event, context):
                 'state':'Fulfilled'
                 
                 }
-    
         },
         "messages": [
             {
                 "contentType": "PlainText",
-#Retornando o payload dos slots
-                "content": f"{slots}"
+                "content": f"{resultado}"
             }
         ]
-    }       
+    }
+            
         return response
+        
+        
+        
