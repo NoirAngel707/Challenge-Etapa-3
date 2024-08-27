373015c30c99b193848de89acaf4ac93291fa00a
Jose  Lucas <97398370+Runado@users.noreply.github.com>
2024-08-27 05:38:03 -0700

    AWS LEX INTENTS

--- AwsLexIntents.py
+++ AwsLexIntents.py
@@ -1,20 +1,29 @@
 import json
 import datetime
 import time
-import whois
+from whois import whois
 from datetime import datetime
 
 def lambda_handler(event, context):
     
+    
     intent = event['sessionState']['intent']['name']
     slots = event['sessionState']['intent']['slots']
+    
     
     if event['invocationSource'] == 'FulfillmentCodeHook':
         celular = slots['celular']['value']['originalValue']
         # Acessando o valor de 'originalValue' do campo 'URL'
         URL = slots['URL']['value']['originalValue']
         # Função para isolar os valores de 'originalValue'
-        resultado = whois.whois(URL)
+        resultado_whois = whois.whois(URL) 
+        dominio = resultado_whois.get("domain_name", "N/A")
+        nome = resultado_whois.get("registrant_name", "N/A")
+        pessoa = resultado_whois.get("person", "N/A")
+        nome = resultado_whois.get("registrant_name", "N/A")
+        cpf = resultado_whois.get("registrant_id", "N/A")
+        email = resultado_whois.get("email", "N/A")
+        pais = resultado_whois.get("country", "N/A")
         response = {
         "sessionState": {
             "dialogAction": {
@@ -30,7 +39,7 @@ def lambda_handler(event, context):
         "messages": [
             {
                 "contentType": "PlainText",
-                "content": f"{resultado}"
+                "content": f"Dominio: {dominio}  Nome: {nome}  Pessoa: {pessoa}  CPF: {cpf}   email: {email}  nacionalidade: {pais}"
             }
         ]
     }
