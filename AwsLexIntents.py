c59aa61567221b700d947593e63404078f77161b
Jose  Lucas <97398370+Runado@users.noreply.github.com>
2024-08-28 21:07:35 -0300

    Codigo hospedado no Lambda que irá interagir com o usuário
    Codigo hospedado em uma função Lambda que irá interagir com o usuário

--- AwsLexIntents.py
+++ AwsLexIntents.py
@@ -1,50 +1,55 @@
 import json
 import datetime
 import time
+import re
 from whois import whois
 from datetime import datetime
 
+
 def lambda_handler(event, context):
-    
-    
     intent = event['sessionState']['intent']['name']
     slots = event['sessionState']['intent']['slots']
-    
-    
+
     if event['invocationSource'] == 'FulfillmentCodeHook':
-        celular = slots['celular']['value']['originalValue']
+        #celular = slots['celular']['value']['originalValue']
         # Acessando o valor de 'originalValue' do campo 'URL'
         URL = slots['URL']['value']['originalValue']
         # Função para isolar os valores de 'originalValue'
-        resultado_whois = whois.whois(URL) 
-        dominio = resultado_whois.get("domain_name", "N/A")
-        nome = resultado_whois.get("registrant_name", "N/A")
-        pessoa = resultado_whois.get("person", "N/A")
-        nome = resultado_whois.get("registrant_name", "N/A")
-        cpf = resultado_whois.get("registrant_id", "N/A")
-        email = resultado_whois.get("email", "N/A")
-        pais = resultado_whois.get("country", "N/A")
+        resultado_whois = whois.whois(URL)
+        #
+        dominio = resultado_whois.get("domain_name", "Informação não disponível")
+        nome = resultado_whois.get("registrant_name", "Informação não disponível")
+        pessoa = resultado_whois.get("person", "Informação não disponível")
+        nome = resultado_whois.get("registrant_name", "Informação não disponível")
+        cpf = resultado_whois.get("registrant_id", "Informação não disponível")
+        email = resultado_whois.get("email", "Informação não disponível")
+        pais = resultado_whois.get("country", "Informação não disponível")
+        data = resultado_whois.get("creation_date", "Informação não disponível")
+        realdata = data.strftime('%Y-%m-%d %H:%M:%S')
+    
+        
+        
+        
         response = {
-        "sessionState": {
-            "dialogAction": {
-                "type": "Close"
-            },
-            "intent": {
-                'name':intent,
-                'slots': slots,
-                'state':'Fulfilled'
-                
+            "sessionState": {
+                "dialogAction": {
+                    "type": "Close"
+                },
+                "intent": {
+                    'name': intent,
+                    'slots': slots,
+                    'state': 'Fulfilled'
+
                 }
-        },
-        "messages": [
-            {
-                "contentType": "PlainText",
-                "content": f"Dominio: {dominio}  Nome: {nome}  Pessoa: {pessoa}  CPF: {cpf}   email: {email}  nacionalidade: {pais}"
-            }
-        ]
-    }
-            
+            },
+            "messages": [
+                {
+                    "contentType": "PlainText",
+                    "content": f"Dominio: {dominio}  Nome: {nome} Pessoa: {pessoa}  CPF: {cpf}  Email: {email}  Pais: {pais}  Data de criação do site: {data} "
+                }
+            ]
+        }
+
         return response
-        
-        
-        
+
+
